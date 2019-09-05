8b9af2cc3f4cc2120c0b52ec4256d7f6ebd378f3
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2019-09-05 16:25:09 +0300

    fix tests

--- test/bigtest/helpers/PluginHarnessSingleSelect.js
+++ test/bigtest/helpers/PluginHarnessSingleSelect.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import noop from 'lodash/noop';
-import { Pluggable, withStripes } from '@folio/stripes/core';
 
-class PluginHarnessSingleSelect extends React.Component {
-  render() {
-    return (
-      <Pluggable
-        aria-haspopup="true"
-        type="find-po-line"
-        id="clickable-find-po-line"
-        searchLabel="Look up po lines"
-        marginTop0
-        searchButtonStyle="link"
-        dataKey="poLinesTest"
-        addLines={noop}
-        isSingleSelect
-        {...this.props}
-      >
-        <span data-test-no-plugin-available>No plugin available!</span>
-      </Pluggable>
-    );
-  }
-}
+import { Pluggable } from '@folio/stripes/core';
 
-export default withStripes(PluginHarnessSingleSelect);
+const PluginHarnessSingleSelect = (props) => (
+  <Pluggable
+    aria-haspopup="true"
+    type="find-po-line"
+    id="clickable-find-po-line"
+    searchLabel="Look up po lines"
+    marginTop0
+    searchButtonStyle="link"
+    addLines={noop}
+    isSingleSelect
+    {...props}
+  >
+    <span data-test-no-plugin-available>No plugin available!</span>
+  </Pluggable>
+);
+
+export default PluginHarnessSingleSelect;
--- test/bigtest/interactors/findPOLines.js
+++ test/bigtest/interactors/findPOLines.js
@@ -8,25 +8,25 @@ import {
 } from '@bigtest/interactor';
 
 @interactor class PluginModalInteractor {
-  static defaultScope = '[data-test-find-po-line-modal]';
+  static defaultScope = '[data-test-find-records-modal]';
 
-  instances = collection('[role=row] a', {
+  instances = collection('[role=group] [role=row]', {
     click: clickable(),
     selectLine: clickable('input[type="checkbox"]'),
   });
 
-  save = scoped('[data-test-find-po-line-modal-save]', {
+  save = scoped('[data-test-find-records-modal-save]', {
     click: clickable(),
     isDisabled: property('disabled'),
   });
 
-  selectAll = scoped('[data-test-find-po-line-modal-select-all]', {
+  selectAll = scoped('[data-test-find-records-modal-select-all]', {
     click: clickable(),
   });
 }
 
 @interactor class FindPOLineInteractor {
-  button = scoped('[data-test-plugin-find-po-line-button]', {
+  button = scoped('[data-test-plugin-find-record-button]', {
     click: clickable(),
     isFocused: is(':focus'),
   });
