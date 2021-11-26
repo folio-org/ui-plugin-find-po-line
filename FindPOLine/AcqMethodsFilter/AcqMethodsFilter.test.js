68d5425314f13d18ae52f0f623942a4bc9bf218e
Yury Saukou <yury_saukou@epam.com>
2021-11-26 12:22:18 +0300

    UIPFPOL-33 Select 'Acquisition method' filter from controlled vocabulary list

---
+++ FindPOLine/AcqMethodsFilter/AcqMethodsFilter.test.js
@@ -0,0 +1,37 @@
+import { cleanup, render, screen } from '@testing-library/react';
+import { noop } from 'lodash';
+import '@folio/stripes-acq-components/test/jest/__mock__';
+
+import AcqMethodsFilter from './AcqMethodsFilter';
+
+const records = [
+  { value: 'Gift', id: '001' },
+  { value: 'Other', id: '002' },
+];
+
+const defaultProps = {
+  id: 'acq-methods',
+  labelId: 'acq-methods',
+  name: 'acq-methods',
+  onChange: noop,
+  activeFilters: [],
+  resources: { acquisitionMethods: { records } },
+};
+
+const renderAcqMethodsFilter = (props = {}) => (render(
+  <AcqMethodsFilter
+    {...defaultProps}
+    {...props}
+  />,
+));
+
+describe('AcqMethodsFilter component', () => {
+  afterEach(cleanup);
+
+  it('should render all passed options', async () => {
+    renderAcqMethodsFilter();
+
+    expect(screen.getByText(records[0].value)).toBeInTheDocument();
+    expect(screen.getByText(records[1].value)).toBeInTheDocument();
+  });
+});
