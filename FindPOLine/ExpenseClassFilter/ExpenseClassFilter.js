47addc0d61f3120203e630a79f84a60144677372
Yury Saukou <yury_saukou@epam.com>
2021-12-10 10:01:02 +0300

    UIOR-870 Update expense class filter to support multi-select (#112)

--- FindPOLine/ExpenseClassFilter/ExpenseClassFilter.js
+++ FindPOLine/ExpenseClassFilter/ExpenseClassFilter.js
@@ -1,18 +1,57 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 
-import { SelectionFilter } from '@folio/stripes-acq-components';
+import { MultiSelectionFilter } from '@folio/stripes/smart-components';
+import { FilterAccordion } from '@folio/stripes-acq-components';
 
 import { useExpenseClassOptions } from './useExpenseClassOptions';
 
-const ExpenseClassFilter = (props) => {
+const ExpenseClassFilter = ({
+  activeFilters,
+  disabled,
+  closedByDefault,
+  id,
+  labelId,
+  name,
+  onChange,
+}) => {
   const options = useExpenseClassOptions();
 
   return (
-    <SelectionFilter
-      {...props}
-      options={options}
-    />
+    <FilterAccordion
+      activeFilters={activeFilters}
+      closedByDefault={closedByDefault}
+      disabled={disabled}
+      id={id}
+      labelId={labelId}
+      name={name}
+      onChange={onChange}
+    >
+      <MultiSelectionFilter
+        ariaLabelledBy={`accordion-toggle-button-${id}`}
+        dataOptions={options}
+        disabled={disabled}
+        id="expense-class-filter"
+        name={name}
+        onChange={onChange}
+        selectedValues={activeFilters}
+      />
+    </FilterAccordion>
   );
+};
+
+ExpenseClassFilter.propTypes = {
+  activeFilters: PropTypes.arrayOf(PropTypes.string),
+  closedByDefault: PropTypes.bool,
+  disabled: PropTypes.bool,
+  id: PropTypes.string.isRequired,
+  labelId: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+ExpenseClassFilter.defaultProps = {
+  closedByDefault: true,
+  disabled: false,
 };
 
 export default ExpenseClassFilter;
