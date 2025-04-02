6d5aa8f1224fce96654edee0fb4f996c384cf2dc
Yury Saukou <yury_saukou@epam.com>
2025-04-02 19:01:55 +0400

    update

--- FindPOLine/OrderLinesSearchConfig.test.js
+++ FindPOLine/OrderLinesSearchConfig.test.js
@@ -1,9 +1,17 @@
-import { getKeywordQuery } from './OrderLinesSearchConfig';
+import {
+  getKeywordQuery,
+  QUERY_INDEX,
+} from './OrderLinesSearchConfig';
 
 const QUERY = 'query';
 
 it('should return keyword query', () => {
-  const keywordQuery = getKeywordQuery(QUERY);
+  const keywordQuery = getKeywordQuery(
+    QUERY,
+    null,
+    null,
+    { [QUERY_INDEX.PRODUCT_IDS]: (q) => `productIds = "${q}*"` },
+  );
 
-  expect(keywordQuery).toBe(`contributors=="*${QUERY}*" or poLineNumber=="*${QUERY}*" or requester=="*${QUERY}*" or titleOrPackage=="*${QUERY}*" or publisher=="*${QUERY}*" or vendorDetail.vendorAccount=="*${QUERY}*" or vendorDetail.referenceNumbers=="*${QUERY}*" or donor=="*${QUERY}*" or selector=="*${QUERY}*" or physical.volumes=="*${QUERY}*" or productIds=="${QUERY}*"`);
+  expect(keywordQuery).toBe(`contributors=="*${QUERY}*" or poLineNumber=="*${QUERY}*" or requester=="*${QUERY}*" or titleOrPackage=="*${QUERY}*" or publisher=="*${QUERY}*" or vendorDetail.vendorAccount=="*${QUERY}*" or vendorDetail.referenceNumbers=="*${QUERY}*" or donor=="*${QUERY}*" or selector=="*${QUERY}*" or physical.volumes=="*${QUERY}*" or productIds = "${QUERY}*"`);
 });
