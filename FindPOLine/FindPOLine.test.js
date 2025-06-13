3bfc19caaf35987bb540a4f6409be91113be3975
Yury Saukou <yury_saukou@epam.com>
2025-06-13 16:10:22 +0400

    Tech debt: increase Jest test coverage

--- FindPOLine/FindPOLine.test.js
+++ FindPOLine/FindPOLine.test.js
@@ -1,28 +1,66 @@
 import { render } from '@folio/jest-config-stripes/testing-library/react';
+import userEvent from '@folio/jest-config-stripes/testing-library/user-event';
 
 import FindPOLine from './FindPOLine';
 
+jest.mock('@rehooks/local-storage', () => ({
+  useLocalStorage: jest.fn(() => [true]),
+  writeStorage: jest.fn(),
+}));
+
 jest.mock('@folio/stripes-acq-components', () => ({
   ...jest.requireActual('@folio/stripes-acq-components'),
+  ExpenseClassFilter: jest.fn(() => <div>ExpenseClassFilter</div>),
+  PluggableDonorsFilter: jest.fn(() => <div>PluggableDonorsFilter</div>),
+  useAcquisitionMethods: jest.fn().mockReturnValue({ acquisitionMethods: [] }),
   useCentralOrderingSettings: jest.fn(() => ({ enabled: false })),
   useFunds: jest.fn().mockReturnValue({ funds: [] }),
+  useOrderLine: jest.fn().mockReturnValue({ orderLine: {} }),
+}));
+
+jest.mock('@folio/stripes-acq-components/lib/hooks', () => ({
+  ...jest.requireActual('@folio/stripes-acq-components/lib/hooks'),
+  useAcquisitionUnits: jest.fn().mockReturnValue({ acquisitionsUnits: [] }),
+  useLocationsQuery: jest.fn().mockReturnValue({ locations: [] }),
+  useExpenseClassOptions: jest.fn().mockReturnValue([]),
+  useOrganization: jest.fn().mockReturnValue({}),
+  useTags: jest.fn().mockReturnValue({ tags: [] }),
+  useTagsConfigs: jest.fn().mockReturnValue({ сonfigs: [] }),
+  useUser: jest.fn().mockReturnValue({}),
 }));
 
 jest.mock('./hooks', () => ({
+  ...jest.requireActual('./hooks'),
+  useFetchOrderLines: jest.fn().mockReturnValue({
+    fetchOrderLines: jest.fn(() => Promise.resolve({ poLines: [], totalRecords: 0 })),
+  }),
   useMaterialTypes: jest.fn().mockReturnValue({ materialTypes: [] }),
-  useFetchOrderLines: jest.fn().mockReturnValue({ fetchOrderLines: jest.fn() }),
+  usePrefixes: jest.fn().mockReturnValue({ prefixes: [] }),
+  useSuffixes: jest.fn().mockReturnValue({ suffixes: [] }),
 }));
 
-const renderFindPOLine = () => (render(
+const renderFindPOLine = () => render(
   <FindPOLine
     addLines={jest.fn}
   />,
-));
+);
 
 describe('FindPOLine component', () => {
-  it('should render find-poLine plugin', async () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render plugin trigger', async () => {
     const { getByText } = renderFindPOLine();
 
-    expect(getByText('ui-plugin-find-po-line.addPOLine')).toBeDefined();
+    expect(getByText('ui-plugin-find-po-line.addPOLine')).toBeInTheDocument();
+  });
+
+  it('should render plugin modal', async () => {
+    const { getByText } = renderFindPOLine();
+
+    await userEvent.click(getByText('ui-plugin-find-po-line.addPOLine'));
+
+    expect(getByText('ui-plugin-find-po-line.modal.title')).toBeInTheDocument();
   });
 });
