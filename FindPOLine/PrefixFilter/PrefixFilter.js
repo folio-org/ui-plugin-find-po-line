1a31ac22dc59e191c70dd14f770a0f4c365a880c
Alexander Sambale <Alexander.Sambale@samxela.eu>
2025-07-08 14:18:59 +0200

    [UIOR-1450] feat: :sparkles: Add hint if a prefix is deprecated

--- FindPOLine/PrefixFilter/PrefixFilter.js
+++ FindPOLine/PrefixFilter/PrefixFilter.js
@@ -8,7 +8,14 @@ import { usePrefixes } from '../hooks';
 function PrefixFilter({ tenantId, ...rest }) {
   const { prefixes } = usePrefixes({ tenantId });
 
-  const options = useMemo(() => prefixes.map(({ name }) => ({ label: name, value: name })), [prefixes]);
+  const options = useMemo(
+    () =>
+      prefixes.map(({ name, deprecated }) => ({
+        label: deprecated ? `${name} (deprecated)` : name,
+        value: name,
+      })),
+    [prefixes],
+  );
 
   return (
     <SelectionFilter
