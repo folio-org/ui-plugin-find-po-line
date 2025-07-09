79842178ae22090038be1bf2d71b7b03ddbbaf1a
Alexander Sambale <Alexander.Sambale@samxela.eu>
2025-07-09 11:10:13 +0200

    [UIOR-1450] refactor: :globe_with_meridians: Add en, de translation for prefixFilter deprecated

--- FindPOLine/PrefixFilter/PrefixFilter.js
+++ FindPOLine/PrefixFilter/PrefixFilter.js
@@ -4,14 +4,20 @@ import { useMemo } from 'react';
 import { SelectionFilter } from '@folio/stripes-acq-components';
 
 import { usePrefixes } from '../hooks';
+import { useIntl } from 'react-intl';
 
 function PrefixFilter({ tenantId, ...rest }) {
   const { prefixes } = usePrefixes({ tenantId });
+  const intl = useIntl();
+
+  const deprecatedText = intl.formatMessage({
+    id: "ui-plugin-find-po-line.filter.prefixFilter.deprecated",
+  });
 
   const options = useMemo(
     () =>
       prefixes.map(({ name, deprecated }) => ({
-        label: deprecated ? `${name} (deprecated)` : name,
+        label: deprecated ? `${name} (${deprecatedText})` : name,
         value: name,
       })),
     [prefixes],
